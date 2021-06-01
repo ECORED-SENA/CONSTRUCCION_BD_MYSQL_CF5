77b82fe951a9984ccc10d732e930cc5ff8ee5260
john <83730668+DevJohn2021@users.noreply.github.com>
2021-06-01 11:54:21 -0500

    ajustes

--- src/config/global.js
+++ src/config/global.js
@@ -79,11 +79,12 @@ export default {
         icono: 'fas fa-sort-alpha-down',
         titulo: 'Glosario',
       },
+      /*
       {
         nombreRuta: 'complementario',
         icono: 'far fa-folder-open',
         titulo: 'Material complementario',
-      },
+      },*/
       {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
